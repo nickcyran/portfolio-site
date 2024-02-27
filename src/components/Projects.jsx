3987f24bbd75d8d28a36fa0f6334e1b3621c0afe
nickcyran <nicyran@gmail.com>
2024-02-27 16:08:05 -0500

    Fixed github logo missing background

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -28,11 +28,11 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
               onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
+              className="bg-black w-8 h-8 rounded-full flex justify-center items-center cursor-pointer">
               <img
                 src={github}
                 alt="github"
-                className="w-1/2 h-1/2 object-contain"
+                className="w-3/4 h-3/4 object-contain"
               />
             </div>
           </div>
