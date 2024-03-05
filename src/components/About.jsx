70fb72199a4c63275bdff2f92274c33cd593bfa6
nickcyran <nicyran@gmail.com>
2024-03-04 22:53:31 -0500

    added back text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,7 @@ const ServiceCard = ({ index, title, icon }) => {
           }}
           className="bg-color-1 rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col ">
 
-          <img src={icon} alt={title} className="w-16 h-16"/>
+          <img src={icon} alt={title} className="w-16 h-16" />
           <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
         </div>
       </motion.div>
@@ -47,7 +47,9 @@ const About = () => {
         variants={isMobile ? {} : fadeIn("", "", 0.1, 1)}
         className={styles.sectionTextBlock}
       >
-        {/* Your existing text */}
+        I'm a computer science student at the University at Albany, with strong foundations in Java, C, JavaScript, and React.
+        I'm deeply passionate about technology, a quick learner, and an excellent problem solver. I love to learn; I am dedicated to the continuous pursuit of new ideas.
+        I'd love to be given the opportunity to work with others and create something meaningful!
       </motion.p>
 
       <div className="mt-16 flex flex-wrap gap-10 justify-center items-center" >
