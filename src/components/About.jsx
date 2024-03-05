3304c04758c2fd80ca242b499d3cdebdfe641ac5
nickcyran <nicyran@gmail.com>
2024-03-04 22:57:29 -0500

    text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -38,7 +38,7 @@ const About = () => {
 
   return (
     <div>
-      <motion.div variants={textVariant()}>
+      <motion.div variants={isMobile ? {} : textVariant()}>
         <p className={styles.sectionSubText} >INTRODUCTION</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
--- src/styles.js
+++ src/styles.js
@@ -4,7 +4,7 @@ const styles = {
     mainText:
       "text-black font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
     mainSubText:
-      "text-black font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+      "text-black font-medium lg:text-[30px] sm:text-[30px] xs:text-[26px] text-[20px] lg:leading-[40px]",
 
     sectionHeadText:
       "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
